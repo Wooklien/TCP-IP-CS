aa8f902f15730a4211a9d1a340ae266c0b633034
Wook LIen <Wooklien@gmail.com>
2014-02-10 23:15:52 -0800

    02.10.14 Update
    Client and Server Code updated.

--- Client.java
+++ Client.java
@@ -12,8 +12,13 @@ public class Client {
 		String server = null;
 		int port = 0;
 		
+		/*
+		 * Setting up server variables, using command line or user input. 
+		 */
 		if(args.length == 0) {
 			System.out.println("No Arguments Passed...");
+			
+			System.out.println("===================================================================");
 			
 			System.out.print("Server: ");
 			server = console.readLine();
@@ -21,37 +26,57 @@ public class Client {
 			System.out.print("Port: ");
 			port = Integer.parseInt(console.readLine());	
 		}
+		else {
+			System.out.println("===================================================================");
+		}
 		
+		//Byte Array with 100kb buffer.
 		int size; //Size of the file.
 		byte[] buffer = new byte[BUFFER];
-		
-		System.out.print("Enter <File>: ");
-		String file = new String(console.readLine());
-		byte[] data = file.getBytes();
 		
 		//Creating Socket that is connected to server on specific port.
 		Socket socket = new Socket(server, port);
 		System.out.println("Connected to server: " + server + ":" + port);
+		System.out.println("===================================================================");
+		
+		//Asking user to input file name. Will save file using input name.
+		System.out.print("Enter <File>: ");
+		String file = new String(console.readLine());
+		byte[] data = file.getBytes();
 		
 		OutputStream out = socket.getOutputStream();
 		
+		//Sending file name to server.
 		out.write(data);
 		
+		//Creating file, with file name and retrieving byte from server.
 		FileOutputStream fos = new FileOutputStream(file + ".txt");
 		BufferedOutputStream bos = new BufferedOutputStream(fos);
 		InputStream in = socket.getInputStream();
 		
 		int totalBytesRcvd = 0;
-				
+		int count = 0;
+		
+		System.out.println("Downloading " + file + ".txt");
+			
+		//Writing buffered array to file.
 		while((size = in.read(buffer)) != -1) {
 			fos.write(buffer, 0, size);
 			totalBytesRcvd += size;
+			count++;
 			
-			System.out.println(totalBytesRcvd + "bytes received.");
+			if(count == 60000){
+				System.out.print("[]");
+				count = 0;
+			}
+			
 		}
 		
-		System.out.println("Recieved File: <" + file + ".txt> | File Size: " + totalBytesRcvd);
+		System.out.println("");
+		System.out.println("\tRecieved File: <" + file + ".txt> | File Size: " + totalBytesRcvd);
+		System.out.println("===================================================================");
 		
+		//Closing connection.
 		in.close();
 		out.close();
 		socket.close();
--- Server.java
+++ Server.java
@@ -14,6 +14,10 @@ public class Server {
 		//Sever Port String - Must Parse to Int before use. 
 		int serverPort = 0;
 		
+		/*
+		 * Server setup using either Command line argument
+		 * or user input.
+		 */
 		if(args.length != 1){
 			System.out.println("No Arguments passed: <PORT>");
 			
@@ -28,39 +32,54 @@ public class Server {
 			serverPort = Integer.parseInt(console.readLine());
 		}
 		
+		System.out.println("===================================================================");
 		System.out.println(":: Server Started :: Listening to Port " + serverPort + " ::");
+		System.out.println("===================================================================");
 		
 		//Create a server socket to accept client connection requests
 		ServerSocket serverSocket = new ServerSocket(serverPort);
 		
 		int size; //Size of the file.
-		byte[] buffer = new byte[BUFFER];
+		byte[] buffer = new byte[BUFFER]; //Byte array of buffer size.
 		
 		while(true) {
+			//Accepting client socket request. 
 			Socket clientSocket = serverSocket.accept();
 			date.getTime();
 			
 			SocketAddress clientAddress = clientSocket.getRemoteSocketAddress();
-			System.out.println("Time: " +  date.toString() + " | Client at " + clientAddress + " has Connected.");
+			System.out.println("Time: " +  new Date().toString() + " | Client at " + clientAddress + " has Connected.");
 			
+			//Getting file name input from client.
 			InputStream inFile = clientSocket.getInputStream();
 			
+			//Setting up a 32 byte array for file name.
 			byte[] data = new byte[32];
 			inFile.read(data);
 			
+			//Creating file object with file name.
 			String fileName = new String(data);
 			File file = new File(fileName);
 			
-			System.out.println("Client Requesting File " + file );
+			System.out.println("Client Requesting File " + file);
 			
+			/*
+			 * Creating a Buffered File Stream to send through socket.  
+			 * Error with setting up a client requested file. (Pathing Error)
+			 */
 			InputStream in = new BufferedInputStream(new FileInputStream("TEST.txt"));
 			OutputStream out = clientSocket.getOutputStream();
 			
+			//Sending File to socket, a Byte at a time.
+			System.out.println("Sending File Request to Client.");
 			while((size = in.read(buffer)) != -1){
 				out.write(buffer);
 			}
 			
-			System.out.println("Time: " +  date.toString() + " | Client at " + clientAddress + " has closed its connection.");
+			//Closing connection.
+			System.out.println("Client has received its request.");
+			System.out.println("Time: " +  new Date().toString() + " | Client at " + clientAddress + " has closed its connection.");
+			System.out.println("===================================================================");
 			in.close();
 			out.close();
 			clientSocket.close();
